8ffa41166748ee7b49b23f98b9d702cd0c65bf1a
Jeni <j.karanesheva@gmail.com>
2012-03-27 17:14:56 +0300

    first commit

---
+++ homework1.php
@@ -0,0 +1,56 @@
+﻿<html>
+
+<head>
+	<title>
+		<?php echo "Домашна работа";  
+		?>
+	</title>
+</head>
+
+<body>
+
+	<form action="homework1.php" method="get">
+		Въведи число: <input type="text" name="number" />
+					  <input type="submit" />
+	</form>
+	
+	<?php
+		function IsPrime($number)
+		{
+			if ($number < 2) 
+			{ 
+					return FALSE;
+			}
+			for ($i=2; $i<=($number / 2); $i++) 
+			{
+					if($number % $i == 0) 
+					{ 
+							return FALSE;
+					}
+			}
+			return TRUE;
+		}
+		
+		function CheckN($number)
+		{
+			if(!(is_numeric($number)))
+			{
+				echo "<font color='red'> The parameter is not a number</font>";
+			}
+			else if (!((0 <= $number) && ($number <= 19999))) 
+			{
+				echo "<font color='red'> The parameter is not within the range [0,19999]</font>";
+			}
+			else if (!(IsPrime($number)))
+			{
+                echo "<font color='blue'> The number " . $number . " is NOT prime!</font>";
+			}
+			else
+				echo "The number " . $number . " is prime!";
+		}
+		
+		echo CheckN($_GET["number"]);
+	?>
+</body>
+
+</html>
---
+++ homework2.php
@@ -0,0 +1,78 @@
+﻿<html>
+
+<head>
+	<title>
+		<?php echo "Домашна работа";  
+		?>
+	</title>
+</head>
+
+<body>
+	
+	<?php
+		$array = range (20, 1000, 37);
+		
+		function IsPrime($number)
+		{
+			if ($number < 2) 
+			{ 
+					return FALSE;
+			}
+			for ($i=2; $i<=($number / 2); $i++) 
+			{
+					if($number % $i == 0) 
+					{ 
+							return FALSE;
+					}
+			}
+			return TRUE;
+		}
+		
+		function find_3_prime()
+		{
+			/*foreach ( $GLOBALS['array'] as $value ) 
+			{
+				echo $value . " ";
+			}*/
+			$count = 0;
+			foreach ($GLOBALS['array'] as $value)
+			{
+				if (IsPrime($value))
+				{
+					++$count;
+					if($count == 3)
+					{
+						echo "The third prime number in array is: " . $value . ";<br>";
+					}
+				}
+			}
+		}
+		
+		function check_exists()
+		{
+			global $array;
+			if (in_array(146, $array))
+			{
+				echo "The number 146 does exist in the array.<br>";
+			}
+			else 
+				echo "The number 146 does not exist in the array.<br>";
+			if (in_array(284, $array))
+			{
+				echo "The number 284 does exist in the array.<br>";
+			}
+			else 
+				echo "The number 284 does not exist in the array.<br>";
+			if (in_array(871, $array))
+			{
+				echo "The number 871 does exist in the array.<br>";
+			}
+			else 
+				echo "The number 871 does not exist in the array.<br>";
+		}
+		find_3_prime();
+		check_exists();
+	?>
+</body>
+
+</html>
